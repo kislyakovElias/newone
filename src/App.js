09e600319bcda96673225cb2a9c9e7aaf5d9655c
superfoxx <super_fox@mail.ru>
2021-10-20 23:14:36 -0700

    api table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import {useEffect, useRef, useState} from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [rep, setRep] = useState([]);
+    const [loader, setLoader] = useState(true);
+    const [page, setPage] = useState(1);
+    const [lastel, setLastel] =useState(null)
+
+    const observer = new IntersectionObserver(
+        entries => {
+            if (entries[0].isIntersecting) {
+                console.log('page increment')
+                setPage(page+1)
+                setLoader(true)
+            }
+        }
+    );
+
+    function fetchData() {
+        axios({
+            method: 'GET',
+            url: 'https://api.github.com/search/repositories',
+            params: {
+                q: `javascript`,
+                sort: 'stars',
+                order: 'desc',
+                page: page,
+            }
+        }).then((res) => {
+            setRep([...rep,...res.data.items])
+            setLoader(false)
+        }).catch((err) => console.log(err))
+    }
+
+
+    useEffect(() => {
+        setTimeout(() => {
+            fetchData()
+        }, 0)
+
+    }, [page])
+
+    useEffect(() => {
+
+        if(lastel){observer.observe(lastel)}
+        return ()=>{ if(lastel){observer.unobserve(lastel)}}
+    }
+        ,[lastel])
+
+
+    return (
+        <div className="App">
+            {/*{loader && <img src={logo}>}*/}
+            {loader ? <img className="App-logo" src={logo}/> :
+                <table className="table">
+                    <tbody className="thead">
+                    <tr className="thead">
+                        <td>Name</td>
+                        <td>url</td>
+                        <td>owner</td>
+                        <td>forks</td>
+                        <td>open issues</td>
+                    </tr>
+                    {rep.map((el) => <tr ref={setLastel} className="tbodey">
+                        <td>{el.name}</td>
+                        <td>{el.url}</td>
+                        <td>{el.owner.login}</td>
+                        <td>{el.forks}</td>
+                        <td>{el.open_issues}</td>
+
+                    </tr>)}
+                    </tbody>
+                </table>
+            }
+        </div>
+    );
 }
 
 export default App;
